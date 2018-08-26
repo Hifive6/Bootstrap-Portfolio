15549da8f15d294d9f1ff3f3a8fc5ef1286af901
Hifive6 <39879994+Hifive6@users.noreply.github.com>
2018-08-26 15:19:08 -0400

    adding validation for name and email

--- app.js
+++ app.js
@@ -19,16 +19,34 @@
 // });
 
 $("#submit").on("click", function() {
-    required();
+    requiredName();
+    requiredEmail();
     
     });
 
-function required()
+function requiredName()
 {
 var empt = $("#valid").val().trim();
+console.log(empt)
 if (empt === "")
 {
 alert("Please input a Value");
+return false;
+}
+else 
+{
+alert('Code has accepted : you can try another');
+return true; 
+}
+}
+
+function requiredEmail()
+{
+var empt = $("#email-Input").val().trim();
+console.log(empt)
+if (empt === "")
+{
+alert("Please an Email");
 return false;
 }
 else 
